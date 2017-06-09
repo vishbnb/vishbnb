1017a9309550617f7b0b8737dc0848b2d61e9cc6
Phil <birkal@outlook.com>
2017-06-09 12:12:43 -0700

    Add hbs views and logger

--- server/app.js
+++ server/app.js
@@ -1,8 +1,34 @@
 const express = require('express');
+const logger = require('morgan');
+const path = require('path');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', {title: 'Vishbnb'});
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(3000, function() {
